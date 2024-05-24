93df9205c91e524f0154639a511a9e128ca2a2b6
c8e4 <c8e4@proton.me>
2024-05-24 18:33:27 +0200

    add Trading page to root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Fonts from "$lib/ui/Fonts.svelte";
+	import Trading from "$lib/ui/trading/Trading.svelte";
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<Trading></Trading>
 <Fonts></Fonts>
--- src/routes/trading/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Footer from "$lib/ui/Footer.svelte";
-	import Header from "$lib/ui/Header.svelte";
-	import Trading from "$lib/ui/trading/Trading.svelte";
-</script>
-
-<Header></Header>
-<Trading></Trading>
-<Footer></Footer>
