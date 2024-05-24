2b0cbf924bade71e6239e64a1d046c3617c88a87
c8e4 <c8e4@proton.me>
2024-05-24 17:34:52 +0200

    add fonts example

---
+++ src/lib/ui/Fonts.svelte
@@ -0,0 +1,15 @@
+<div class="font1">Markets</div>
+
+<style lang="postcss">
+    
+    .font1{
+        font-weight: 500;
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Helvetica,Arial,sans-serif;
+        background-color: var(--bg-secondary);
+        color: var(--title);
+        font-size: 14px;
+        font-feature-settings: "tnum";
+        font-variant: tabular-nums;
+        line-height: 1.5715;
+    }
+</style>
