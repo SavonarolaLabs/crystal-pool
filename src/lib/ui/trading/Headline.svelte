b29b64c56b47b57c8eea2dd980368b6589a53de8
Savonarola <x@x.com>
2024-05-24 20:53:31 +0400

    headline fonts wip

--- src/lib/ui/trading/Headline.svelte
+++ src/lib/ui/trading/Headline.svelte
@@ -1,8 +1,90 @@
-<div class="headline">
-    headline
+<div class="headline_wrapper">
+	<div class="header_wrapper">
+		<div class="headline_bigName">BTC/USDT</div>
+		<div class="headline_etfDisplay">Bitcoin</div>
+		<div class="headline_latestPrice">68,163.62</div>
+
+		<div class="headline_change">-0.11%</div>
+		<div class="headline_changeDown">-0.11%</div>
+
+		<div class="headline_leftItem">24h High</div>
+
+		<div class="headline_etfDisplay">68,158.39</div>
+	</div>
 </div>
+
 <style lang="postcss">
-    .headline{
-        background-color: pink;
-    }
+	.headline {
+		background-color: pink;
+	}
+
+	.headline_wrapper {
+		justify-content: space-between;
+		-webkit-padding-start: 10px;
+		padding-inline-start: 10px;
+		height: 100%;
+		color: var(--text-secondary);
+	}
+
+	.header_wrapper {
+		font-weight: 500;
+		font-family:
+			-apple-system,
+			BlinkMacSystemFont,
+			Segoe UI,
+			Roboto,
+			Helvetica Neue,
+			Helvetica,
+			Arial,
+			sans-serif;
+		background-color: var(--bg-secondary);
+		color: var(--title);
+		font-size: 14px;
+		font-feature-settings: 'tnum';
+		font-variant: tabular-nums;
+		line-height: 1.5715;
+	}
+
+	.headline_bigName {
+		line-height: 20px;
+		font-size: 16px;
+		font-weight: 700;
+		color: var(--text-primary);
+		//margin: 0;
+	}
+	.headline_latestPrice {
+		font-size: 16px;
+		font-weight: 700;
+		color: var(--text-primary);
+	}
+
+	.headline_etfDisplay {
+		font-size: 12px;
+		font-weight: 400;
+		color: var(--text-secondary);
+		max-width: 140px;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		cursor: pointer;
+		-webkit-margin-end: 10px;
+		margin-inline-end: 10px;
+	}
+
+	.headline_leftItem {
+		-webkit-margin-end: 15px;
+		margin-inline-end: 15px;
+		font-size: 12px;
+		line-height: 20px;
+		white-space: nowrap;
+	}
+
+	.headline_change {
+		color: var(--title);
+		-webkit-margin-start: 4px;
+		margin-inline-start: 4px;
+	}
+	.headline_changeDown {
+		color: var(--down);
+	}
 </style>
--- src/lib/ui/trading/Trading.svelte
+++ src/lib/ui/trading/Trading.svelte
@@ -1,55 +1,65 @@
 <script>
-	import Actions from "./Actions.svelte";
-	import Chart from "./Chart.svelte";
-	import Headline from "./Headline.svelte";
-	import Markets from "./Markets.svelte";
-	import OrderBook from "./OrderBook.svelte";
-	import Orders from "./Orders.svelte";
-	import Trades from "./Trades.svelte";
+	import Actions from './Actions.svelte';
+	import Chart from './Chart.svelte';
+	import Headline from './Headline.svelte';
+	import Markets from './Markets.svelte';
+	import OrderBook from './OrderBook.svelte';
+	import Orders from './Orders.svelte';
+	import Trades from './Trades.svelte';
 </script>
+
 <div class="exchange">
-    <!-- left -->
-    <div class="orderbook">
-        <OrderBook></OrderBook>
-    </div>
-    <!-- top center -->
-    <div class="headline">
-        <Headline></Headline>
-    </div>
-    <!-- top center -->
-    <div class="chart">
-        <Chart></Chart>
-    </div>
-    <!-- top right -->
-    <div class="markets">
-        <Markets></Markets>
-    </div>
-    <!-- bottom center -->
-    <div class="actions">
-        <Actions></Actions>
-    </div>
-    <!-- bottom right -->
-    <div class="trades">
-        <Trades></Trades>
-    </div>
-    <!-- bottom -->
-    <div class="orders">
-        <Orders></Orders>
-    </div>
+	<!-- left -->
+	<div class="orderbook">
+		<OrderBook></OrderBook>
+	</div>
+	<!-- top center -->
+	<div class="headline">
+		<Headline></Headline>
+	</div>
 </div>
 
 <style lang="postcss">
-    .exchange{
-        display: grid;
-        grid-column-gap: 5px;
-        grid-row-gap: 5px;
-        justify-items: stretch;
-        align-items: stretch;
-        padding: 5px 0;
-        max-width: 1520px;
-        min-width: 760px;
-        margin: 0 auto;
-        grid-template-columns: minmax(350px,1fr) minmax(auto,2.5fr) minmax(380px,1fr);
-        grid-template-rows: 44px 410px 380px 330px;
-    }
+	.exchange {
+		display: grid;
+		grid-column-gap: 5px;
+		grid-row-gap: 5px;
+		justify-items: stretch;
+		align-items: stretch;
+		padding: 5px 0;
+		max-width: 1520px;
+		min-width: 760px;
+		margin: 0 auto;
+		grid-template-columns: minmax(350px, 1fr) minmax(auto, 2.5fr) minmax(
+				380px,
+				1fr
+			);
+		grid-template-rows: 44px 410px 380px 330px;
+	}
+
+	.headline_bigName {
+		line-height: 20px;
+		font-size: 16px;
+		font-weight: 700;
+		color: var(--text-primary);
+		//margin: 0;
+	}
+	.headline_latestPrice {
+		font-size: 16px;
+		font-weight: 700;
+		color: var(--text-primary);
+	}
+
+	.headline_etfDisplay {
+		font-size: 12px;
+		font-weight: 400;
+		color: var(--text-secondary);
+		max-width: 140px;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		cursor: pointer;
+		-webkit-margin-end: 10px;
+		margin-inline-end: 10px;
+	}
 </style>
