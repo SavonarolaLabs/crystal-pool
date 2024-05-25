2cf619de2ee29b283de53e0de7c114bef8b16501
c8e4 <c8e4@proton.me>
2024-05-25 20:15:12 +0200

    footer styling

--- src/lib/ui/Footer.svelte
+++ src/lib/ui/Footer.svelte
@@ -1,5 +1,8 @@
+<script>
+	import Network from "./footer/Network.svelte";
+</script>
 <div class="footer">
-	<div>Service Connected</div>
+	<Network></Network>
 	<div class="flex gap-2">
 		<div>Github</div>
 		<div>KYA</div>
@@ -8,13 +11,17 @@
 
 <style lang="postcss">
 	.footer {
+		border-top: 1px solid var(--divider-primary);
+		background-color: var(--bg-level-secondary);
 		display: flex;
+		align-items: center;
 		justify-content: space-between;
 		padding-left: 16px;
 		padding-right: 16px;
 		position: fixed;
+		height: 32px;
+    	margin: 0 auto;
 		bottom: 0;
 		width: 100%;
-		background-color: greenyellow;
 	}
 </style>
---
+++ src/lib/ui/footer/Network.svelte
@@ -0,0 +1,47 @@
+<div class="flex items-center network_stable_color">
+	<svg
+		focusable="false"
+		width="1em"
+		height="1em"
+		fill="currentColor"
+		aria-hidden="true"
+		viewBox="0 0 20 20"
+		data-icon="currentColor"
+		><rect
+			x="2"
+			y="11.7969"
+			width="2.72718"
+			height="5.19982"
+			fill="currentColor"
+		></rect><rect
+			x="6.09082"
+			y="9.20312"
+			width="2.72718"
+			height="7.79972"
+			fill="currentColor"
+		></rect><rect
+			x="10.1807"
+			y="6.60156"
+			width="2.72718"
+			height="10.3996"
+			fill="currentColor"
+		></rect><rect
+			x="14.2725"
+			y="4"
+			width="2.72718"
+			height="12.9995"
+			fill="currentColor"
+		></rect></svg
+	>
+	<div class="ml-1 network-status-text">Connected</div>
+</div>
+
+<style lang="postcss">
+.network-status-text{
+    font-size: 12px;
+    line-height: 14px;
+}
+.network_stable_color{
+    color: var(--primary-color);
+}
+</style>
