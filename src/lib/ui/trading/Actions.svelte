6e6599ae05e1ff51f1ae176c9faa96665d1bd5f1
c8e4 <c8e4@proton.me>
2024-05-26 01:11:33 +0200

    add actions header

--- src/lib/ui/trading/Actions.svelte
+++ src/lib/ui/trading/Actions.svelte
@@ -1,8 +1,41 @@
 <div class="actions">
-    actions
+	<div class="actions_header">
+		<div class="actions_headerTabActive">
+				Spot
+		</div>
+        <div class="fee">
+            Maker <span style="display: inline-block; direction: ltr;"
+                >0.000%</span
+            >
+            / Taker
+            <span style="display: inline-block; direction: ltr;"
+                >0.000%</span
+            >
+        </div>
+	</div>
 </div>
+
 <style lang="postcss">
-    .actions{
-        background-color: pink;
+	.actions {
+	}
+    .actions_header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        border-bottom: 1px solid var(--divider-primary);
+        padding: 0 16px;
+        flex-shrink: 0;
+        line-height: 40px;
+    }
+    .actions_headerTabActive{
+        font-weight: 600;
+    }
+    .fee{
+        color: var(--tint-blue-base);
+        background-color: var(--tint-blue-smooth);
+        font-size: 12px;
+        border-radius: 2px;
+        padding: 0 6px;
+        line-height: 20px;
     }
 </style>
