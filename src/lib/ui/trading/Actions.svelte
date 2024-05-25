b5cdd310d7465623eb9e001260ee455ee4ccb95c
c8e4 <c8e4@proton.me>
2024-05-26 01:33:02 +0200

    actions widget wip

--- src/lib/ui/trading/Actions.svelte
+++ src/lib/ui/trading/Actions.svelte
@@ -1,41 +1,52 @@
 <div class="actions">
 	<div class="actions_header">
-		<div class="actions_headerTabActive">
-				Spot
+		<div class="actions_headerTabActive">Spot</div>
+		<div class="fee">
+			Maker <span style="display: inline-block; direction: ltr;"
+				>0.000%</span
+			>
+			/ Taker
+			<span style="display: inline-block; direction: ltr;">0.000%</span>
 		</div>
-        <div class="fee">
-            Maker <span style="display: inline-block; direction: ltr;"
-                >0.000%</span
-            >
-            / Taker
-            <span style="display: inline-block; direction: ltr;"
-                >0.000%</span
-            >
+	</div>
+	<div class="actions_contentWrapper">
+            <div
+            class="actions_line actions_mode__nRnKJ actions_textNowarp__3QcjB actions_modeActive__VpeUM"
+        >
+            Limit
         </div>
 	</div>
 </div>
 
 <style lang="postcss">
 	.actions {
+		background-color: var(--bg-level-secondary);
 	}
-    .actions_header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        border-bottom: 1px solid var(--divider-primary);
-        padding: 0 16px;
-        flex-shrink: 0;
-        line-height: 40px;
-    }
-    .actions_headerTabActive{
-        font-weight: 600;
-    }
-    .fee{
-        color: var(--tint-blue-base);
-        background-color: var(--tint-blue-smooth);
-        font-size: 12px;
-        border-radius: 2px;
-        padding: 0 6px;
-        line-height: 20px;
+	.actions_header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		border-bottom: 1px solid var(--divider-primary);
+		padding: 0 16px;
+		flex-shrink: 0;
+		line-height: 40px;
+	}
+	.actions_headerTabActive {
+		font-weight: 600;
+	}
+	.fee {
+		color: var(--tint-blue-base);
+		background-color: var(--tint-blue-smooth);
+		font-size: 12px;
+		border-radius: 2px;
+		padding: 0 6px;
+		line-height: 20px;
+	}
+	.actions_contentWrapper {
+		padding: 5px 16px 13px;
+	}
+    .actions_line {
+        margin-bottom: 10px;
+        margin-top: 5px;
     }
 </style>
