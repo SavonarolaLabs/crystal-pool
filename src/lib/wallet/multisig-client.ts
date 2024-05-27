72de1dac16fdbaf312cb2ef44dfe9b30e3cf567a
c8e4 <c8e4@proton.me>
2024-05-27 20:12:34 +0200

    wip Buffer works

--- src/lib/wallet/multisig-client.ts
+++ src/lib/wallet/multisig-client.ts
@@ -28,7 +28,6 @@ export async function signTxMulti(
 		await signMultisig(unsignedTx, userMnemonic, userAddress)
 	).to_js_eip12();
 }
-const bip32 = BIP32Factory(ecc);
 
 type JSONTransactionHintsBag = any;
 
@@ -346,6 +345,7 @@ export async function signTxInput(
 const getWalletAddressSecret = (mnemonic: string, idx: number = 0) => {
 	let seed = mnemonicToSeedSync(mnemonic);
 	const path = calcPathFromIndex(idx);
+	const bip32 = BIP32Factory(ecc);
 	const extended = bip32.fromSeed(seed).derivePath(path);
 	return wasm.SecretKey.dlog_from_bytes(
 		Uint8Array.from(extended.privateKey ?? Buffer.from(''))
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+		import { Buffer } from 'buffer';
 	import '../app.css';
 	import '../mxui.css';
+
+	onMount(()=>{
+
+		// Make Buffer globally available
+		(window as any).Buffer = Buffer;
+	})
+
 </script>
 
 <slot />
