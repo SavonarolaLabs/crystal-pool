fd31d8c829f6d75d4cecc18b067bfe1417f2275d
c8e4 <c8e4@proton.me>
2024-05-28 18:42:16 +0200

    /order-books/rsBTC_sigUSD

---
+++ src/server/orderBooks.ts
@@ -0,0 +1,19 @@
+import type { FastifyInstance, FastifyRequest } from 'fastify';
+import type { BoxDB } from '$lib/db/db';
+
+interface OrderBooksParams {
+  tradingPair: string;
+}
+
+export function orderBooksRoute(fastify: FastifyInstance, opts: any, done: any) {
+  fastify.get('/order-books/:tradingPair', async (request: FastifyRequest<{ Params: OrderBooksParams }>, reply) => {
+    const db: BoxDB = fastify.db;
+    const { tradingPair } = request.params;
+
+    const filteredBoxRows = db.boxRows.filter(boxRow => boxRow.id === tradingPair);
+    
+    return filteredBoxRows;
+  });
+
+  done();
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -4,6 +4,7 @@ import { sumRoute } from './libraryA';
 import { processNewSwap, processNewSwapSign } from './swapOrder';
 import { boxesRoute } from './boxes';
 import { initDb } from '$lib/db/db';
+import { orderBooksRoute } from './orderBooks';
 
 const fastify = Fastify({ logger: true });
 
@@ -20,12 +21,24 @@ let db;
 const start = async () => {
 	try {
     db = initDb();
+
+    const boxRow  = {
+      id: "rsBTC_sigUSD",
+      box: "bigbox"
+  }
+  const boxRow2  = {
+    id: "foobar",
+    box: "bigbox"
+}
+    db.boxRows.push(boxRow)
+    db.boxRows.push(boxRow2)
     fastify.decorate('db', db)
 
     fastify.register(sumRoute);
     fastify.register(processNewSwap);
     fastify.register(processNewSwapSign);
     fastify.register(boxesRoute);
+    fastify.register(orderBooksRoute);
 
 		await fastify.listen({ port: 3000 }); // Updated to use object for listen method
 		fastify.log.info(`Server running at http://localhost:3000`);
