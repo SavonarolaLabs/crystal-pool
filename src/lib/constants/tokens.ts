715281fcac674b846450f51bb8f160b00bc7ad2f
Savonarola <x@x.com>
2024-05-29 18:59:25 +0400

    add tokens decimals

--- src/lib/constants/tokens.ts
+++ src/lib/constants/tokens.ts
@@ -1,12 +1,23 @@
-export const TOKEN_ID_rsBTC  = '5bf691fbf0c4b17f8f8cece83fa947f62f480bfbd242bd58946f85535125db4d';
-export const TOKEN_ID_sigUSD = '03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04';
+export const TOKEN = {
+	rsBTC: {
+		tokenId:
+			'5bf691fbf0c4b17f8f8cece83fa947f62f480bfbd242bd58946f85535125db4d',
+		name: 'rsBTC',
+		decimals: 9,
+		type: 'EIP-004'
+	},
+	sigUSD: {
+		tokenId:
+			'03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04',
+		name: 'sigUSD', //?
+		decimals: 9, //?
+		type: 'EIP-004'
+	}
+};
 
 export const tradingPairs = [
-    {
-        name: 'rsBTC_sigUSD',
-        tokens: [
-            TOKEN_ID_rsBTC,
-            TOKEN_ID_sigUSD
-        ]
-    }
+	{
+		name: 'rsBTC_sigUSD',
+		tokens: [TOKEN.rsBTC.tokenId, TOKEN.sigUSD.tokenId]
+	}
 ];
