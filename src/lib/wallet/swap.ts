e5b85837d97025454c89156277d79abb88919f28
Savonarola <x@x.com>
2024-05-30 16:24:46 +0400

    new f createSwapOrderTxR9

--- src/lib/wallet/swap.ts
+++ src/lib/wallet/swap.ts
@@ -1,7 +1,26 @@
-import { SHADOWPOOL_ADDRESS, SWAP_ORDER_ADDRESS } from "$lib/constants/addresses";
-import { first, type Amount, type Box, type EIP12UnsignedTransaction, type OneOrMore } from "@fleet-sdk/common";
-import { ErgoAddress, OutputBuilder, RECOMMENDED_MIN_FEE_VALUE, SAFE_MIN_BOX_VALUE, SColl, SGroupElement, SInt, SSigmaProp, TransactionBuilder } from "@fleet-sdk/core";
-import { SByte, SLong, SPair } from "@fleet-sdk/serializer";
+import {
+	SHADOWPOOL_ADDRESS,
+	SWAP_ORDER_ADDRESS
+} from '$lib/constants/addresses';
+import {
+	first,
+	type Amount,
+	type Box,
+	type EIP12UnsignedTransaction,
+	type OneOrMore
+} from '@fleet-sdk/common';
+import {
+	ErgoAddress,
+	OutputBuilder,
+	RECOMMENDED_MIN_FEE_VALUE,
+	SAFE_MIN_BOX_VALUE,
+	SColl,
+	SGroupElement,
+	SInt,
+	SSigmaProp,
+	TransactionBuilder
+} from '@fleet-sdk/core';
+import { SByte, SLong, SPair } from '@fleet-sdk/serializer';
 
 export function createSwapOrderTx(
 	sellerPK: string,
@@ -11,8 +30,8 @@ export function createSwapOrderTx(
 	sellRate: bigint,
 	currentHeight: number,
 	unlockHeight: number,
-    sellingTokenId: string,
-    buyingTokenId: string,
+	sellingTokenId: string,
+	buyingTokenId: string
 ): EIP12UnsignedTransaction {
 	const output = new OutputBuilder(SAFE_MIN_BOX_VALUE, SWAP_ORDER_ADDRESS)
 		.addTokens(token)
@@ -39,6 +58,67 @@ export function createSwapOrderTx(
 				SByte,
 				ErgoAddress.fromBase58(sellerMultisigAddress).ergoTree
 			).toHex()
+		});
+
+	const unsignedTransaction = new TransactionBuilder(currentHeight)
+		.from(inputBoxes)
+		.to(output)
+		.sendChangeTo(sellerPK)
+		.payFee(RECOMMENDED_MIN_FEE_VALUE)
+		.build()
+		.toEIP12Object();
+	return unsignedTransaction;
+}
+
+function splitSellRate(sellRate: string): [number, number] {
+	let floatRate = parseFloat(sellRate);
+	let exponent = 0;
+	while (floatRate % 1 !== 0) {
+		floatRate *= 10;
+		exponent -= 1;
+	}
+	return [floatRate, exponent];
+}
+
+export function createSwapOrderTxR9(
+	sellerPK: string,
+	sellerMultisigAddress: string,
+	inputBoxes: OneOrMore<Box<Amount>>,
+	token: { tokenId: string; amount: Amount },
+	sellRate: string,
+	currentHeight: number,
+	unlockHeight: number,
+	sellingTokenId: string,
+	buyingTokenId: string
+): EIP12UnsignedTransaction {
+	const [integerPart, exponentPart] = splitSellRate(sellRate);
+
+	const output = new OutputBuilder(SAFE_MIN_BOX_VALUE, SWAP_ORDER_ADDRESS)
+		.addTokens(token)
+		.setAdditionalRegisters({
+			R4: SColl(SSigmaProp, [
+				SGroupElement(
+					first(ErgoAddress.fromBase58(sellerPK).getPublicKeys())
+				),
+				SGroupElement(
+					first(
+						ErgoAddress.fromBase58(
+							SHADOWPOOL_ADDRESS
+						).getPublicKeys()
+					)
+				)
+			]).toHex(),
+			R5: SInt(unlockHeight).toHex(),
+			R6: SPair(
+				SColl(SByte, sellingTokenId),
+				SColl(SByte, buyingTokenId)
+			).toHex(),
+			R7: SLong(BigInt(integerPart)).toHex(),
+			R8: SColl(
+				SByte,
+				ErgoAddress.fromBase58(sellerMultisigAddress).ergoTree
+			).toHex(),
+			R9: SInt(exponentPart).toHex()
 		});
 
 	const unsignedTransaction = new TransactionBuilder(currentHeight)
