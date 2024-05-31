197b3a9da00f16bf8944ec81c556fcc5df5da1e0
Savonarola <x@x.com>
2024-05-31 23:33:07 +0400

    change Swap Contract

--- src/lib/tests/swap.spec.ts
+++ src/lib/tests/swap.spec.ts
@@ -17,7 +17,8 @@ import { utxos } from '$lib/data/utxos';
 import { boxesAtAddress, boxesAtAddressUnsigned } from '$lib/utils/test-helper';
 import {
 	signMultisigEIP12,
-	signTxInput
+	signTxInput,
+	signTxMulti
 } from '$lib/wallet/multisig-server';
 import {
 	ErgoAddress,
@@ -27,144 +28,171 @@ import {
 } from '@fleet-sdk/core';
 import * as wasm from 'ergo-lib-wasm-nodejs';
 import { describe, expect, it } from 'vitest';
-import { createSwapOrderTxR9, executeSwap, createSwapOrderTx } from '../wallet/swap';
+import {
+	createSwapOrderTxR9,
+	executeSwap,
+	createSwapOrderTx
+} from '../wallet/swap';
 import { TOKEN } from '$lib/constants/tokens';
 import { sumAssetsFromBoxes } from '$lib/utils/helper';
 import { parseBox } from '$lib/db/db';
 import type { BoxParameters, ContractType } from '$lib/types/boxRow';
 
-
 const CONTRACT_FOR_TEST = `{	
-		def getSellerPk(box: Box)              	= box.R4[Coll[SigmaProp]].getOrElse(Coll[SigmaProp](sigmaProp(false),sigmaProp(false)))(0)
-		def getPoolPk(box: Box)                	= box.R4[Coll[SigmaProp]].getOrElse(Coll[SigmaProp](sigmaProp(false),sigmaProp(false)))(1)
-		def unlockHeight(box: Box)             	= box.R5[Int].get
-		def getSellingTokenId(box: Box)        	= box.R6[(Coll[Byte],Coll[Byte])].getOrElse((Coll[Byte](),Coll[Byte]()))._1
-		def getBuyingTokenId(box: Box)         	= box.R6[(Coll[Byte],Coll[Byte])].getOrElse((Coll[Byte](),Coll[Byte]()))._2
-		def getRate(box: Box)                  	= box.R7[Long].get
-		def getSellerMultisigAddress(box: Box)  = box.R8[Coll[Byte]].get
+	def getSellerPk(box: Box)              	= box.R4[Coll[SigmaProp]].getOrElse(Coll[SigmaProp](sigmaProp(false),sigmaProp(false)))(0)
+	def getPoolPk(box: Box)                	= box.R4[Coll[SigmaProp]].getOrElse(Coll[SigmaProp](sigmaProp(false),sigmaProp(false)))(1)
+	def unlockHeight(box: Box)             	= box.R5[Int].get
+	def getSellingTokenId(box: Box)        	= box.R6[(Coll[Byte],Coll[Byte])].getOrElse((Coll[Byte](),Coll[Byte]()))._1
+	def getBuyingTokenId(box: Box)         	= box.R6[(Coll[Byte],Coll[Byte])].getOrElse((Coll[Byte](),Coll[Byte]()))._2
+	def getRate(box: Box)                  	= box.R7[Long].get
+	def getSellerMultisigAddress(box: Box)  = box.R8[Coll[Byte]].get
+  def getDenom(box: Box)                  = box.R9[Long].get
+
+
+
+	def tokenId(box: Box) = box.tokens(0)._1
+	def tokenAmount(box: Box) = box.tokens(0)._2
+
+
+//------------------------
+	def isSameContract(box: Box) = 
+		box.propositionBytes == SELF.propositionBytes
+
+	def isSameTokenPair (box: Box) = 
+		getSellingTokenId(SELF) == getSellingTokenId(box) &&
+		getBuyingTokenId(SELF)  == getBuyingTokenId(box)
+
 	
-		def tokenId(box: Box) = box.tokens(0)._1
-		def tokenAmount(box: Box) = box.tokens(0)._2
-		  def sumTokenAmount(a:Long, b: Box) = a + tokenAmount(b)
-		  def sumTokenAmountXRate(a:Long, b: Box) = a + tokenAmount(b) * getRate(b)   
+  def hasSellingToken(box: Box) = 
+		getSellingTokenId(SELF) == getSellingTokenId(box) &&
+		box.tokens.size > 0 &&
+		getSellingTokenId(SELF) == tokenId(box)
+
+	def hasBuyingToken(box: Box) = 
+		getBuyingTokenId(SELF) == getBuyingTokenId(box) &&
+		box.tokens.size > 0 &&
+		getBuyingTokenId(SELF) == tokenId(box)
+
+  	def isGreaterZeroRate(box:Box) =
+		getRate(box) > 0
+
+	def isSameSeller(box: Box)   = 
+		getSellerPk(SELF) == getSellerPk(box) &&
+		getPoolPk(SELF) == getPoolPk(box)
+
+  	def isSameUnlockHeight(box: Box)  = 
+		unlockHeight(SELF) == unlockHeight(box)
+
+ 	def isSameMultisig(box: Box)    =
+		getSellerMultisigAddress(SELF) == getSellerMultisigAddress(box)
+
+	def isLegitInput(box: Box) =
+		isSameContract(box) &&
+		isSameSeller(box) &&
+		isSameUnlockHeight(box) && 
+		isSameTokenPair(box) &&
+		hasSellingToken(box) &&
+		isGreaterZeroRate(box) &&
+		isSameMultisig(box)
+//-------------------------
+  val maxDenom: Long = INPUTS
+		.filter(isLegitInput)
+		.fold(0L, {(r:Long, box:Box) => {
+			if(r > getDenom(box)) r else getDenom(box)
+		}}) // TAKE MAX DENOM
+  
+    def getRateInMaxDenom(box:Box) = getRate(box)*maxDenom/getDenom(box) //1>
+
+  	def sumTokenAmount(a:Long, b: Box) = a + tokenAmount(b)
+  	def sumTokenAmountXRate(a:Long, b: Box) = a + tokenAmount(b) * getRateInMaxDenom(b)   // <---------------- REWORK WITH MAX DENOM
+
+    val maxSellRate: Long = INPUTS
+      .filter(isLegitInput)
+      .fold(0L, {(r:Long, box:Box) => {
+        if(r > getRateInMaxDenom(box)) r else getRateInMaxDenom(box)
+      }})
+
+      def hasMaxSellRate(box: Box) =
+    getRate(box)*maxDenom==getDenom(box)*maxSellRate //include denom
+
+  	def isLegitSellOrderOutput(box: Box) =
+	  	isLegitInput(box)&&
+	  	hasMaxSellRate(box)
+
+  //-------------------------
+	def isPaymentBox(box:Box) =
+		isSameSeller(box) &&
+		isSameUnlockHeight(box) &&
+		hasBuyingToken(box) &&
+		getSellerMultisigAddress(SELF) == box.propositionBytes
+
+	def sumSellTokensIn(boxes: Coll[Box]): Long = boxes
+		.filter(isLegitInput) 
+		.fold(0L, sumTokenAmount)
+
+	def sumSellTokensOut(boxes: Coll[Box]): Long = boxes
+		.filter(isLegitSellOrderOutput)
+		.fold(0L, sumTokenAmount)
+
+	def sumBuyTokensPaid(boxes: Coll[Box]): Long = boxes
+		.filter(isPaymentBox) 
+		.fold(0L, sumTokenAmount)
+//-------------------------
+
+//-----------------
+  	val tokensSold = sumSellTokensIn(INPUTS) - sumSellTokensOut(OUTPUTS) //rsBTC on contract (delta) (10000)
+
+  	val tokensPaid = sumBuyTokensPaid(OUTPUTS) //sigUSD PAID (20) + ADD DENOM (1000) = > 20_000 
+
+    	val inSellTokensXRate = INPUTS  //VOLUME INPUT ON CONTRACT
+		.filter(isLegitInput) 
+		.fold(0L, sumTokenAmountXRate)   // 2*10000
+
+     	val outSellTokensXRate = OUTPUTS //VOLUME OUTPUT ON CONTRACT 
+		.filter(isLegitSellOrderOutput)
+		.fold(0L, sumTokenAmountXRate)  // Учитывает макс деном  
+
+    val sellTokensXRate = inSellTokensXRate - outSellTokensXRate  // DELTA VOLUME в Макс деноме
+    val expectedRate = sellTokensXRate / tokensSold   // 23125124 in DENOM MAX
+
+    val isPaidAtFairRate = maxDenom*tokensPaid/tokensSold >= expectedRate  //sigUSD PAID (20) + ADD DENOM (1000) = > 20_000. * MAX_DENOM
+    //1000
+    //20
+    //10000
+    //>=
+    //2
 	
-		def isSameContract(box: Box) = 
-			box.propositionBytes == SELF.propositionBytes
-	
-		def isSameTokenPair (box: Box) = 
-			getSellingTokenId(SELF) == getSellingTokenId(box) &&
-			getBuyingTokenId(SELF)  == getBuyingTokenId(box)
-	
-		def hasSellingToken(box: Box) = 
-			getSellingTokenId(SELF) == getSellingTokenId(box) &&
-			box.tokens.size > 0 &&
-			getSellingTokenId(SELF) == tokenId(box)
-	
-		def hasBuyingToken(box: Box) = 
-			getBuyingTokenId(SELF) == getBuyingTokenId(box) &&
-			box.tokens.size > 0 &&
-			getBuyingTokenId(SELF) == tokenId(box)
-	
-		  def isGreaterZeroRate(box:Box) =
-			getRate(box) > 0
-	
-		def isSameSeller(box: Box)   = 
-			getSellerPk(SELF) == getSellerPk(box) &&
-			getPoolPk(SELF) == getPoolPk(box)
-	
-		  def isSameUnlockHeight(box: Box)  = 
-			unlockHeight(SELF) == unlockHeight(box)
-	
-		 def isSameMultisig(box: Box)    =
-			getSellerMultisigAddress(SELF) == getSellerMultisigAddress(box)
-	
-		def isLegitInput(box: Box) =
-			isSameContract(box) &&
-			isSameSeller(box) &&
-			isSameUnlockHeight(box) && 
-			isSameTokenPair(box) &&
-			hasSellingToken(box) &&
-			isGreaterZeroRate(box) &&
-			isSameMultisig(box)
-	
-		val maxSellRate: Long = INPUTS
-			.filter(isLegitInput)
-			.fold(0L, {(r:Long, box:Box) => {
-				if(r > getRate(box)) r else getRate(box)
-			}})
-	
-		  def hasMaxSellRate(box: Box) =
-			getRate(box) == maxSellRate
-	
-		  def isLegitSellOrderOutput(box: Box) =
-			  isLegitInput(box)&&
-			  hasMaxSellRate(box)
-	
-		def ispayedBox(box:Box) =
-			isSameSeller(box) &&
-			isSameUnlockHeight(box) &&
-			hasBuyingToken(box) &&
-			getSellerMultisigAddress(SELF) == box.propositionBytes
-	
-		def sumSellTokensIn(boxes: Coll[Box]): Long = boxes
-			.filter(isLegitInput) 
-			.fold(0L, sumTokenAmount)
-	
-		def sumSellTokensOut(boxes: Coll[Box]): Long = boxes
-			.filter(isLegitSellOrderOutput)
-			.fold(0L, sumTokenAmount)
-	
-		def sumBuyTokensPaid(boxes: Coll[Box]): Long = boxes
-			.filter(ispayedBox) 
-			.fold(0L, sumTokenAmount)
-	  
-		  val tokensSold = sumSellTokensIn(INPUTS) - sumSellTokensOut(OUTPUTS)
-		  val tokensPaid = sumBuyTokensPaid(OUTPUTS)
-	
-		val inSellTokensXRate = INPUTS
-			.filter(isLegitInput) 
-			.fold(0L, sumTokenAmountXRate)
-	
-		  val outSellTokensXRate = OUTPUTS
-			.filter(isLegitSellOrderOutput)
-			.fold(0L, sumTokenAmountXRate)
-	
-		val sellTokensXRate = inSellTokensXRate - outSellTokensXRate
-		val expectedRate = sellTokensXRate / tokensSold   
-	
-		  val isPaidAtFairRate = tokensPaid/tokensSold >= expectedRate
-	
-		if(HEIGHT > unlockHeight(SELF)){
-			getSellerPk(SELF)
-		}else{
-			getSellerPk(SELF) && getPoolPk(SELF) || sigmaProp(isPaidAtFairRate) && getPoolPk(SELF)
-		}
-	}`;
+  if(HEIGHT > unlockHeight(SELF)){
+		getSellerPk(SELF)
+	}else{
+    getSellerPk(SELF) && getPoolPk(SELF) || sigmaProp(isPaidAtFairRate) && getPoolPk(SELF)
+	}
+}`;
+
 const CONTRACT_WITH_R9 = compileContract(CONTRACT_FOR_TEST);
 let height = 1_275_000;
 let unlockHeight = 1300000;
 
 function customRecognizer(box: Box): ContractType {
-    const address = new ErgoTree(box.ergoTree).toAddress().toString();
-    if (address == DEPOSIT_ADDRESS) {
-        return 'DEPOSIT';
-    } else if (address == BUY_ORDER_ADDRESS) {
-        return 'BUY';
-    } else if (address == SELL_ORDER_ADDRESS) {
-        return 'SELL';
-    } else if (address == CONTRACT_WITH_R9) {
-        return 'SWAP';
-    } else {
-        return 'UNKNOWN';
-    }
+	const address = new ErgoTree(box.ergoTree).toAddress().toString();
+	if (address == DEPOSIT_ADDRESS) {
+		return 'DEPOSIT';
+	} else if (address == BUY_ORDER_ADDRESS) {
+		return 'BUY';
+	} else if (address == SELL_ORDER_ADDRESS) {
+		return 'SELL';
+	} else if (address == CONTRACT_WITH_R9) {
+		return 'SWAP';
+	} else {
+		return 'UNKNOWN';
+	}
 }
 
-function parseBoxCustom(box: Box): BoxParameters | undefined{
+function parseBoxCustom(box: Box): BoxParameters | undefined {
 	return parseBox(box, customRecognizer);
 }
 
 describe('New Swap order with R9', async () => {
-
 	const tokenForSale = {
 		tokenId: TOKEN.rsBTC.tokenId,
 		price: '0.002',
@@ -172,7 +200,7 @@ describe('New Swap order with R9', async () => {
 	};
 	const paymentToken = {
 		tokenId: TOKEN.sigUSD.tokenId,
-		amount: 20n,
+		amount: 20n
 	};
 
 	it.only('Order + deposit[ALICE], change:ALICE: FULL execute', async () => {
@@ -189,14 +217,20 @@ describe('New Swap order with R9', async () => {
 			CONTRACT_WITH_R9
 		);
 
-		const bobSwapTx = await signMultisigEIP12(unsignedTx1, BOB_MNEMONIC, BOB_ADDRESS);
+		const bobSwapTx = await signTxMulti(
+			unsignedTx1,
+			BOB_MNEMONIC,
+			BOB_ADDRESS
+		);
 		const swapOrderBoxes = boxesAtAddress(bobSwapTx, CONTRACT_WITH_R9);
 		expect(swapOrderBoxes, 'swap order boxes').toBeDefined();
 		expect(swapOrderBoxes.length, 'swap order boxes length').toBe(1);
 
 		const paymentInTokens = {
 			tokenId: paymentToken.tokenId,
-			amount: BigInt(Number(tokenForSale.amount) * Number(tokenForSale.price))
+			amount: BigInt(
+				Number(tokenForSale.amount) * Number(tokenForSale.price)
+			)
 		};
 
 		const executeSwapOrderTx = executeSwap(
@@ -207,25 +241,45 @@ describe('New Swap order with R9', async () => {
 			paymentInTokens,
 			SAFE_MIN_BOX_VALUE
 		);
-		expect(boxesAtAddressUnsigned(executeSwapOrderTx, DEPOSIT_ADDRESS).length).toBe(2);
-		expect(boxesAtAddressUnsigned(executeSwapOrderTx, SWAP_ORDER_ADDRESS).length).toBe(0);
-		expect(parseBoxCustom(executeSwapOrderTx.inputs[0])?.contract).toBe('SWAP');
-		expect(parseBoxCustom(executeSwapOrderTx.inputs[0])?.parameters.userPk).toBe(BOB_ADDRESS);
-		expect(parseBoxCustom(executeSwapOrderTx.inputs[0])?.parameters.poolPk).toBe(SHADOWPOOL_ADDRESS);
-		expect(parseBoxCustom(executeSwapOrderTx.inputs[0])?.parameters.unlockHeight).toBe(1300000);
-		
+		expect(
+			boxesAtAddressUnsigned(executeSwapOrderTx, DEPOSIT_ADDRESS).length
+		).toBe(2);
+		expect(
+			boxesAtAddressUnsigned(executeSwapOrderTx, SWAP_ORDER_ADDRESS)
+				.length
+		).toBe(0);
+		expect(parseBoxCustom(executeSwapOrderTx.inputs[0])?.contract).toBe(
+			'SWAP'
+		);
+		expect(
+			parseBoxCustom(executeSwapOrderTx.inputs[0])?.parameters.userPk
+		).toBe(BOB_ADDRESS);
+		expect(
+			parseBoxCustom(executeSwapOrderTx.inputs[0])?.parameters.poolPk
+		).toBe(SHADOWPOOL_ADDRESS);
+		expect(
+			parseBoxCustom(executeSwapOrderTx.inputs[0])?.parameters
+				.unlockHeight
+		).toBe(1300000);
+
+		expect(
+			parseBoxCustom(executeSwapOrderTx.inputs[1])?.parameters
+				.unlockHeight
+		).toBe(1300000);
+
 		const signedBobInput = await signTxInput(
 			SHADOW_MNEMONIC,
 			executeSwapOrderTx,
 			0
 		);
-		expect(signedBobInput, "bob can sign index:0").toBeDefined();
+		expect(signedBobInput, 'bob can sign index:0').toBeDefined();
 
-		expect(parseBoxCustom(executeSwapOrderTx.inputs[1])?.contract).toBe('DEPOSIT')
+		expect(parseBoxCustom(executeSwapOrderTx.inputs[1])?.contract).toBe(
+			'DEPOSIT'
+		);
 
 		//const signed = signMultisigEIP12(executeSwapOrderTx, ALICE_MNEMONIC, ALICE_ADDRESS);
 		//expect(signed).toBeDefined();
-
 
 		return;
 
@@ -296,4 +350,3 @@ describe('New Swap order with R9', async () => {
 		console.log(unsignedTx.id);
 	});
 });
-
