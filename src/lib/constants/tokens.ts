bee35a916d75019a6a7ee800ba4285c4ad12f83b
Savonarola <x@x.com>
2024-06-01 21:42:08 +0400

    add fake sigUSD

--- src/lib/constants/tokens.ts
+++ src/lib/constants/tokens.ts
@@ -1,14 +1,12 @@
 export const TOKEN = {
 	rsBTC: {
-		tokenId:
-			'5bf691fbf0c4b17f8f8cece83fa947f62f480bfbd242bd58946f85535125db4d',
+		tokenId: '5bf691fbf0c4b17f8f8cece83fa947f62f480bfbd242bd58946f85535125db4d',
 		name: 'rsBTC',
 		decimals: 9,
 		type: 'EIP-004'
 	},
 	sigUSD: {
-		tokenId:
-			'03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04',
+		tokenId: 'f60bff91f7ae3f3a5f0c2d35b46ef8991f213a61d7f7e453d344fa52a42d9f9a',
 		name: 'sigUSD', //?
 		decimals: 2, //?
 		type: 'EIP-004'
@@ -17,7 +15,7 @@ export const TOKEN = {
 
 export const pairName = {
 	rsBTC_sigUSD: 'rsBTC_sigUSD'
-}
+};
 
 export const tradingPairs = [
 	{
