7e9f1bf54f79085b704fafc8d0ee07e0c2f7375d
c8e4 <c8e4@proton.me>
2024-06-08 11:33:41 +0200

    toaster offset

--- src/lib/ui/toaster.ts
+++ src/lib/ui/toaster.ts
@@ -3,6 +3,7 @@ import toast from 'svelte-french-toast';
 export const showToast = (message:string, type = 'default') => {
   toast.success(message, {
     duration: 4000,
+    className: "mt-10",
     position: 'top-right',
   });
 };
