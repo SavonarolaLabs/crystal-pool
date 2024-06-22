e939243bb9d9860ca80954ce70562cd21d1af5fd
c8e4 <c8e4@proton.me>
2024-06-22 21:54:07 +0200

    deposit page

--- src/routes/assets/deposit/+page.svelte
+++ src/routes/assets/deposit/+page.svelte
@@ -3,47 +3,77 @@
 		<div class="back-arrow">&#8592;</div>
 		<div class="title">Deposit</div>
 	</div>
-	<div class="deposit_container">
-		<div class="deposit_dot">Select Crypto</div>
-		<div class="select-token_wrapper">
-			<div class="select-token_selectMode">
-				<div class="flex items-center gap-2">
-					<img
-						src="https://www.mexc.com/api/file/download/F20210514192151938ROhGjOFp2Fpgb7"
-					/><span class="select-token_currency">rsBTC</span><label>Bitcoin on Steroids</label>
-				</div>
-				<svg
-					class="-mr-1 h-5 w-5 text-gray-400"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					aria-hidden="true"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</div>
-		</div>
+	<div class="deposit_container flex items-center">
+        <div class=" grow">
+            <div class="deposit_dot">Select Token</div>
+            <div class="select-token_wrapper">
+                <div class="select-token_selectMode">
+                    <div class="flex items-center gap-2">
+                        <img
+                            src="https://www.mexc.com/api/file/download/F20210514192151938ROhGjOFp2Fpgb7"
+                        /><span class="select-token_currency">rsBTC</span><label
+                            >Bitcoin on Steroids</label
+                        >
+                    </div>
+                    <svg
+                        class="-mr-1 h-5 w-5 text-gray-400"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </div>
+            </div>
+
+            <div class="deposit_dot">Enter Amount</div>
+            <div class="select-token_wrapper">
+                <input class="w-full ant-input ant-input-lg">
+            </div>
+
+            <div class="select-token_wrapper">
+                <button class="btn">Deposit</button>
+            </div>
+        </div>
+        <div class="grow flex justify-center">
+            <img style="background:white;max-width:300px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1920px-QR_code_for_mobile_English_Wikipedia.svg.png" alt="">
+        </div>
 	</div>
 </div>
 
 <style lang="postcss">
-    .select-token_currency{
-        ont-size: 14px;
-    font-weight: 600;
-    }
-	.select-token_wrapper img{
+    .btn {
+		height: 48px;
+		width: 100%;
+		border: none;
+		color: #fff;
+		border-radius: 4px;
+		cursor: pointer;
+		transition: all 0.16s ease-in;
+        background-color: #3b82f6;
+	}
+	.select-token_wrapper {
+		margin-bottom: 40px;
+        max-width: 500px;
+	}
+	.select-token_currency {
+		ont-size: 14px;
+		font-weight: 600;
+	}
+	.select-token_wrapper img {
 		width: 24px;
 		height: 24px;
 		border-radius: 100%;
 	}
-    .select-token_wrapper label{
-        font-size: 14px;
-    color: var(--text-secondary);
-    opacity: .6;
-    }
+	.select-token_wrapper label {
+		font-size: 14px;
+		color: var(--text-secondary);
+		opacity: 0.6;
+	}
 
 	.select-token_selectMode {
 		height: 48px;
