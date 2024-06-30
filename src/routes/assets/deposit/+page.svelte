619c28e93b7bb9cd214550a9424c58a873e00f5f
c8e4 <c8e4@proton.me>
2024-06-30 15:29:26 +0200

    select wallet tabs

--- src/routes/assets/deposit/+page.svelte
+++ src/routes/assets/deposit/+page.svelte
@@ -1,72 +1,87 @@
 <script>
-	import { goto } from "$app/navigation";
-	import { ergoTokens } from "$lib/constants/ergoTokens";
-	import SelectCrypto from "$lib/ui/assets/SelectCrypto.svelte";
+	import { goto } from '$app/navigation';
+	import { ergoTokens } from '$lib/constants/ergoTokens';
+	import SelectCrypto from '$lib/ui/assets/SelectCrypto.svelte';
 
 	let tokenId = '03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04';
 
 	let selectCryptoDialogOpen = false;
-	function selectCrypto(){
+	function selectCrypto() {
 		selectCryptoDialogOpen = true;
 	}
-	function handleMessage(event){
-		tokenId = event.detail.coin
+	function handleMessage(event) {
+		tokenId = event.detail.coin;
 	}
 </script>
+
 <SelectCrypto bind:showDialog={selectCryptoDialogOpen} on:message={handleMessage}></SelectCrypto>
 <div class="flex flex-col items-center">
 	<div class="page-header flex items-center">
-		<button class="back-arrow" on:click={()=>goto("/assets")}>&#8592;</button>
+		<button class="back-arrow" on:click={() => goto('/assets')}>&#8592;</button>
 		<div class="title">Deposit</div>
 	</div>
 	<div class="deposit_container flex items-center">
-        <div class=" grow">
-            <div class="deposit_dot">Select Crypto</div>
-            <div class="select-token_wrapper">
-                <!-- svelte-ignore a11y-click-events-have-key-events -->
-                <!-- svelte-ignore a11y-no-static-element-interactions -->
-                <div class="select-token_selectMode" on:click={selectCrypto}>
-                    <div class="flex items-center gap-2">
+		<div class=" grow">
+			<div class="deposit_dot">Select Wallet</div>
+			<div class="tabs select-token_wrapper">
+				<input type="radio" id="htmlTwo" name="fav_language_two" value="HTMLTwo" checked />
+				<label for="htmlTwo">Mobile</label>
+				<input type="radio" id="cssTwo" name="fav_language_two" value="CSSTwo" />
+				<label for="cssTwo">Web3 Wallet</label>
+			</div>
+
+			<div class="deposit_dot">Select Crypto</div>
+			<div class="select-token_wrapper">
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div class="select-token_selectMode" on:click={selectCrypto}>
+					<div class="flex items-center gap-2">
 						<img
-						style="width:32px;"
-						alt=""
-						src="{ergoTokens[tokenId].logoURI?ergoTokens[tokenId].logoURI:`/token/${tokenId}.svg`}"
-					/><span class="select-token_currency">{ergoTokens[tokenId].ticker}</span><span class="label"
-                            >{ergoTokens[tokenId].name}</span>
-                    </div>
-                    <svg
-                        class="-mr-1 h-5 w-5 text-gray-400"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                </div>
-            </div>
+							style="width:32px;"
+							alt=""
+							src={ergoTokens[tokenId].logoURI
+								? ergoTokens[tokenId].logoURI
+								: `/token/${tokenId}.svg`}
+						/><span class="select-token_currency">{ergoTokens[tokenId].ticker}</span
+						><span class="label">{ergoTokens[tokenId].name}</span>
+					</div>
+					<svg
+						class="-mr-1 h-5 w-5 text-gray-400"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						aria-hidden="true"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</div>
+			</div>
 
-            <div class="deposit_dot">Enter Amount</div>
-            <div class="select-token_wrapper">
-                <input class="w-full ant-input ant-input-lg">
-            </div>
+			<div class="deposit_dot">Enter Amount</div>
+			<div class="select-token_wrapper">
+				<input class="w-full ant-input ant-input-lg" />
+			</div>
 
-            <div class="select-token_wrapper">
-                <button class="btn" on:click={selectCrypto}>Deposit</button>
-            </div>
-        </div>
-        <div class="grow flex justify-center">
-            <img style="background:white;max-width:300px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1920px-QR_code_for_mobile_English_Wikipedia.svg.png" alt="">
-        </div>
+			<div class="select-token_wrapper">
+				<button class="btn" on:click={selectCrypto}>Deposit</button>
+			</div>
+		</div>
+		<div class="grow flex justify-center">
+			<img
+				style="background:white;max-width:300px;"
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1920px-QR_code_for_mobile_English_Wikipedia.svg.png"
+				alt=""
+			/>
+		</div>
 	</div>
 </div>
 <SelectCrypto></SelectCrypto>
 
 <style lang="postcss">
-    .btn {
+	.btn {
 		height: 48px;
 		width: 100%;
 		border: none;
@@ -74,11 +89,11 @@
 		border-radius: 4px;
 		cursor: pointer;
 		transition: all 0.16s ease-in;
-        background-color: #3b82f6;
+		background-color: #3b82f6;
 	}
 	.select-token_wrapper {
 		margin-bottom: 40px;
-        max-width: 500px;
+		max-width: 500px;
 	}
 	.select-token_currency {
 		ont-size: 14px;
