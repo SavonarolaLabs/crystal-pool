b99fe328fa05213862fd820d2f7a572bb69aa8ec
c8e4 <c8e4@proton.me>
2024-07-14 18:50:30 +0200

    rename

--- src/lib/external/headers.ts
+++ src/lib/external/headers.ts
@@ -1,16 +1,16 @@
-export async function headers() {
-    return fetch('https://gql.ergoplatform.com/', {
-        method: 'POST',
-        headers: {
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Connection': 'keep-alive',
-            'DNT': '1',
-            'Origin': 'https://gql.ergoplatform.com'
-        },
-        body: JSON.stringify({
-            query: `
+export async function fetchHeaders() {
+	return fetch('https://gql.ergoplatform.com/', {
+		method: 'POST',
+		headers: {
+			'Accept-Encoding': 'gzip, deflate, br',
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			Connection: 'keep-alive',
+			DNT: '1',
+			Origin: 'https://gql.ergoplatform.com'
+		},
+		body: JSON.stringify({
+			query: `
                 query Headers($take: Int) {
                     blockHeaders(take: $take) {
                         headerId
@@ -29,16 +29,17 @@ export async function headers() {
                     }
                 }
             `,
-            variables: {
-                take: 10
-            }
-        }),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => data.data.blockHeaders); // Extracting the 'blockHeaders' array from the response data
+			variables: {
+				take: 10
+			}
+		})
+	})
+		.then((response) => {
+			if (!response.ok) {
+				//throw new Error('Network response was not ok');
+				return [];
+			}
+			return response.json();
+		})
+		.then((data) => data.data.blockHeaders);
 }
